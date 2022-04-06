53bbb8c682192081464f5ac237bfce804622a77c
eycanny <nancy.the.coder@gmail.com>
2022-04-05 21:42:22 -0700

    Created a number validation event listener

--- main.js
+++ main.js
@@ -6,6 +6,23 @@ colorChanger.addEventListener('click', () => {
     const colorChange = document.querySelectorAll('.color-change');
     
     for (const element of colorChange) {
-        element.classList.add('red');
+    element.classList.add('red');
+    }
+});
+
+const numberForm = document.querySelector('.number-form')
+
+numberForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+
+    const numberInput = document.querySelector('#number-input');
+    const userNum = Number(numberInput.value);
+
+    const formFeedback = document.querySelector('#formFeedback');
+
+    if (userNum >= 10 || isNaN(userNum)) {
+        formFeedback.innerHTML = 'Please enter a smaller number.';
+    } else {
+        formFeedback.innerHTML = 'You\'re good to go!';
     }
 });
