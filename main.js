4d18a457b460d5f72f29a0d56ce11de0d0541190
eycanny <nancy.the.coder@gmail.com>
2022-04-05 20:16:29 -0700

    Created a color changing event listener

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const colorChanger = document.querySelector('.color-changer');
+
+colorChanger.addEventListener('click', () => {
+    const colorChange = document.querySelectorAll('.color-change');
+    
+    for (const element of colorChange) {
+        element.classList.add('red');
+    }
+});
